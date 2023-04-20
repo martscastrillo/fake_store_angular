1cf38b5fee2d68b641ce231e5dc9be11ad461d53
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-20 14:51:15 +0200

    add propertt blinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-//estas variables deben tener un
+//estas variables deben tener un acceso publico
 export class AppComponent {
   name = 'Marta';
   age = 32;
+  img = 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-624x385@Las%20Provincias.jpg';
+  btnDisable = true;
+  person = {
+    name: 'Marta',
+    age: 32,
+    avatar: 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-624x385@Las%20Provincias.jpg'
+  }
 }
