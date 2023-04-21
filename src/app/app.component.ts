17db58c20f8d64e24e42c6f7d85a1c6c912420fd
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-21 10:32:21 +0200

    add event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 //estas variables deben tener un acceso publico
 export class AppComponent {
   name = 'Marta';
   age = 32;
-  img = 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-624x385@Las%20Provincias.jpg';
+  img =
+    'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-624x385@Las%20Provincias.jpg';
   btnDisable = true;
   person = {
     name: 'Marta',
     age: 32,
-    avatar: 'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-624x385@Las%20Provincias.jpg'
+    avatar:
+      'https://s1.ppllstatics.com/lasprovincias/www/multimedia/202112/12/media/cortadas/gatos-kb2-U160232243326NVC-624x385@Las%20Provincias.jpg',
+  };
+  toggleButton() {
+    this.btnDisable = !this.btnDisable;
+  }
+  ageIncrease(){
+    this.person.age += 1;
   }
 }
