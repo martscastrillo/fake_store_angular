2c2212b884c14d7b0b4a90e1c2f35ef0cae5f80c
Marta Castrillo <mart.castrillo@gmail.com>
2023-04-21 11:06:11 +0200

    add data binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,13 @@ export class AppComponent {
   ageIncrease(){
     this.person.age += 1;
   }
+  onScroll(event : Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+    
+  }
+  changeName(event : Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
